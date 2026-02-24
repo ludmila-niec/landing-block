211a40b3d73608026d37d2d72a8083f0703cae0a
Ludmila Nieczyporuk <ludmila.lyiu@gmail.com>
2026-02-24 20:11:21 -0300

    feat: fixed section, intro and about

---
+++ components/AboutSection.tsx
@@ -0,0 +1,16 @@
+function AboutSection() {
+    return (
+        <div className="w-full h-[100vh] bg-white flex">
+            <div className="w-1/2 h-full bg-violet-700"></div>
+            <div className="w-1/2 h-full bg-violet-700 text-white p-13">
+                <h2 className="text-4xl font-bold uppercase leading-none mt-14">WE PUT STRATEGIC RELEVANCE IN SERVICE OF CREATIVE INSPIRATION.</h2>
+                <p className="text-2xl mt-14 max-w-[70%] mx-auto">
+                    We believe that thinking with clarity and relevance is what enables bold action — and builds brands that truly resonate. Whether in brand strategy or communication, we craft strategic foundations built on relevant insights and distinctive ideas.
+                    The result: strategies that spark action, stimulate reflection, and generate real impact aligned with your business ambitions.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutSection;
---
+++ components/IntroSections.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useEffect, useRef } from "react";
+function IntroSections() {
+    const interactiveRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        console.log("interactiveRef", interactiveRef.current);
+        if (!interactiveRef.current) return;
+
+        const interBubble = interactiveRef.current as HTMLDivElement;
+        let curX = 0;
+        let curY = 0;
+        let tgX = 0;
+        let tgY = 0;
+        let animationFrameId: number | null = null;
+        let isActive = true;
+
+        function move() {
+            if (!isActive) return;
+            curX += (tgX - curX) / 20;
+            curY += (tgY - curY) / 20;
+            interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+            animationFrameId = requestAnimationFrame(move);
+        }
+
+        const handleMouseMove = (event: MouseEvent) => {
+            tgX = event.clientX;
+            tgY = event.clientY;
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+        move();
+
+        return () => {
+            isActive = false;
+            window.removeEventListener('mousemove', handleMouseMove);
+            if (animationFrameId !== null) {
+                cancelAnimationFrame(animationFrameId);
+            }
+        };
+    }, []);
+    return (
+        <div className="h-screen w-full overflow-hidden gradient-bg relative">
+
+            <svg className="hidden" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <filter id="blob-effect">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                        <feBlend in="SourceGraphic" in2="goo" />
+                    </filter>
+                </defs>
+            </svg>
+            <div className="w-full h-full blur-2xl filter-[url(#blob-effect)]">
+                <div className="g1"></div>
+                <div className="g2"></div>
+                <div className="g3"></div>
+                <div className="g4"></div>
+                <div className="g5"></div>
+                <div className="interactive" ref={interactiveRef}></div>
+            </div>
+            {/* <div className="w-1/2 h-full p-13 flex flex-col justify-end z-10 absolute bottom-0 right-0">
+                <h1 className="text-4xl font-bold uppercase text-white indent-13 leading-none bg-white/10">RELEVANT IDEAS AND BOLD ACTIONS TO GENERATE THE MOMENTUM THAT PROPELS YOUR BRAND FORWARD.</h1>
+            </div> */}
+        </div>
+    );
+}
+
+export default IntroSections;
